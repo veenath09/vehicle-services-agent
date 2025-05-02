3c293b221396fc274d581b9178155ca5f63e4eb6
veenath09 <veenathwickramaarchchi@gmail.com>
2025-05-02 13:54:00 +0530

    updated the agent to make reservation of it's own

--- agents/technician.py
+++ agents/technician.py
@@ -11,6 +11,6 @@ from tools.technician_tools import find_technicians, check_technician_availabili
 
 llm = get_llm()
 technician_agent_tools = [find_technicians, check_technician_availability, reserve_technician]
-technician_agent = create_react_agent(llm.bind_tools(technician_agent_tools), tools=[find_technicians,check_technician_availability])
+technician_agent = create_react_agent(llm.bind_tools(technician_agent_tools), tools=[find_technicians,check_technician_availability,reserve_technician])
 
 
